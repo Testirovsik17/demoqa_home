df3deb6e7669de33d7ba40cb5d3a268d3ed93655
testirovsik17 <endmitrenko17@gmail.com>
2025-02-27 03:39:23 +0300

    HW 6 commit_3

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,17 +1,9 @@
 from pages.swag_labs import SwagLabs
 
-def test_find_element1(browser):
+def test_find_element(browser):
     swag_labs_page = SwagLabs(browser)
     swag_labs_page.visit()
     assert swag_labs_page.exist_icon()
-
-def test_find_element2(browser):
-    swag_labs_page = SwagLabs(browser)
-    swag_labs_page.visit()
     assert swag_labs_page.exist_icon('#user-name')
-
-def test_find_element3(browser):
-    swag_labs_page = SwagLabs(browser)
-    swag_labs_page.visit()
     assert swag_labs_page.exist_icon('#password')
 
